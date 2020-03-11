ec78b11d9b33ba9d74122622ba16197dccb546e2
Stefano Perrone <stefano.perrone@capgemini.com>
2020-03-11 18:18:24 +0100

    ex_00: added README and updated gitignore

---
+++ ex_00-start/README.md
@@ -0,0 +1,11 @@
+## Node Course
+
+1. **Javascript**
+   
+    realizzazione di un app con esecuzione con node.js
+    
+    in questo primo esempio vedremo l'utilizzo dei moduli, essi possono essere:
+    
+    - standard
+    - custom
+     
--- ex_00-start/app.js
+++ ex_00-start/app.js
@@ -2,25 +2,25 @@
 const os = require("os");
 const fs = require("fs");
 
-// esempio modulo creato da noi
-const infoStudent = require("./students.js.js");
+// esempio modulo
+const infoStudent = require("./students.js");
 
 console.log("student name: ", infoStudent.student.name);
 
 // info sui moduli usati nel progetto
-// console.log(module);
+console.log(module);
 
 let user = os.userInfo();
 let platform = os.platform();
 
-/* console.log(user);
+console.log(user);
 console.log(user.username);
-console.log(platform); */
+console.log(platform);
 
 let date = new Date();
 let alert = `user ${user.username} started app at date: ${date}\n`;
 
-fs.appendFile("text.txt", alert, function(error) {
+fs.appendFile("node-console.log", alert, function(error) {
   if (error) {
     console.log("Error occurred");
   }
